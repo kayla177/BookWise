1c7a8393e8ad12176bfde5a05f8ecb1e6a06d10a
Kayla <likayla177@gmail.com>
2025-02-26 16:30:28 -0500

    edit email send duration condition logic, and email content

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import React, { ReactNode } from "react";
 import Header from "@/components/Header";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
@@ -9,6 +13,32 @@ const Layout = async ({ children }: { children: ReactNode }) => {
   if (!session) {
     redirect("/sign-in");
   }
+
+  // after we check if the user are sign in
+  //
+  after(async () => {
+    // additional check if the user is logged in
+    if (!session?.user?.id) return;
+
+    // get the user and see if the last activity dat is today
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10)) {
+      // if the last activity date is today, don't record
+      return;
+    }
+    // if the user is logged in, update the database
+    // the slice(0,10) here is takes the day, month and year, we do not need the time here
+    // eq() --> we only want to update the date for the user that is currently logged in
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
 
   return (
     <main className="root-container">
--- app/api/workflow/onboarding/route.ts
+++ app/api/workflow/onboarding/route.ts
@@ -1,17 +1,62 @@
 import { serve } from "@upstash/workflow/nextjs";
+import { db } from "@/database/drizzle";
+import { eq } from "drizzle-orm";
+import { users } from "@/database/schema";
+import { sendEmail } from "@/lib/workflow";
 
 // from https://upstash.com/docs/workflow/examples/customerOnboarding
 
+type UserState = "non-active" | "active";
+
 type InitialData = {
   email: string;
+  fullName: string;
+};
+
+const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+const THREE_DAYS_IN_MS = 3 * ONE_DAY_IN_MS;
+const THIRTY_DAYS_IN_MS = 30 * ONE_DAY_IN_MS;
+
+// return the type "UserState"
+const getUserState = async (email: string): Promise<UserState> => {
+  //   step 1 : get a single user
+  const user = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email))
+    .limit(1);
+
+  if (user.length === 0) {
+    return "non-active";
+  }
+
+  //  step 2: get the time difference that the user login
+  // add the "!" to let typeScript know it will be there, avoid error
+  const lastActivityDate = new Date(user[0].lastActivityDate!);
+  const now = new Date();
+  const timeDifference = now.getTime() - lastActivityDate.getTime();
+
+  // step 3: write conditional logic to get the user state
+  if (
+    timeDifference > THREE_DAYS_IN_MS &&
+    timeDifference <= THIRTY_DAYS_IN_MS
+  ) {
+    return "non-active";
+  }
+
+  return "active";
 };
 
 export const { POST } = serve<InitialData>(async (context) => {
-  const { email } = context.requestPayload;
+  const { email, fullName } = context.requestPayload;
 
-  // 1. send an email to new sign-iup user
+  // 1. send an welcome email to new sign-up user
   await context.run("new-signup", async () => {
-    await sendEmail("Welcome to the platform", email);
+    await sendEmail({
+      email,
+      subject: "Welcome to the platform",
+      message: `Welcome ${fullName}`,
+    });
   });
 
   // 2. To leave time for the user to interact with our platform,
@@ -22,16 +67,24 @@ export const { POST } = serve<InitialData>(async (context) => {
   //     the user’s engagement level with our platform and send appropriate emails:
   while (true) {
     const state = await context.run("check-user-state", async () => {
-      return await getUserState();
+      return await getUserState(email);
     });
 
     if (state === "non-active") {
       await context.run("send-email-non-active", async () => {
-        await sendEmail("Email to non-active users", email);
+        await sendEmail({
+          email,
+          subject: "Are you still there?",
+          message: `Hey ${fullName}, we miss you! 🥰🥰`,
+        });
       });
     } else if (state === "active") {
       await context.run("send-email-active", async () => {
-        await sendEmail("Send newsletter to active users", email);
+        await sendEmail({
+          email,
+          subject: "Welcome back!",
+          message: `Welcome back ${fullName}, good to see you! 🥳🥳`,
+        });
       });
     }
 
@@ -39,15 +92,3 @@ export const { POST } = serve<InitialData>(async (context) => {
     await context.sleep("wait-for-1-month", 60 * 60 * 24 * 30);
   }
 });
-
-async function sendEmail(message: string, email: string) {
-  // Implement email sending logic here
-  console.log(`Sending ${message} email to ${email}`);
-}
-
-type UserState = "non-active" | "active";
-
-const getUserState = async (): Promise<UserState> => {
-  // Implement user state logic here
-  return "non-active";
-};
