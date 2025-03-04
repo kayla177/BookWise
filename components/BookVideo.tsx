29b71e86937d69bd61aa7ad9a21ae91289f36054
Kayla <likayla177@gmail.com>
2025-03-04 16:47:30 -0500

    book detail page

---
+++ components/BookVideo.tsx
@@ -0,0 +1,18 @@
+"use client";
+// IKVideo has to be a client component
+
+import React from "react";
+import { IKVideo, ImageKitProvider } from "imagekitio-next";
+import config from "@/lib/config";
+
+const BookVideo = ({ videoUrl }: { videoUrl: string }) => {
+  return (
+    <ImageKitProvider
+      publicKey={config.env.imagekit.publicKey}
+      urlEndpoint={config.env.imagekit.urlEndpoint}
+    >
+      <IKVideo path={videoUrl} controls={true} className="w-full rounded-xl" />
+    </ImageKitProvider>
+  );
+};
+export default BookVideo;
