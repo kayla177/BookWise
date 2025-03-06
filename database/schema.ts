0ed1264ce70b4cbd4d114110197e8b72b450585b
Kayla <likayla177@gmail.com>
2025-03-06 17:55:41 -0500

    connect to vercel, edit env setting

--- database/schema.ts
+++ database/schema.ts
@@ -24,10 +24,10 @@ export const BORROW_STATUS_ENUM = pgEnum("borrow_status", [
 
 export const users = pgTable("users", {
   id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
-  fullName: varchar("full_name").notNull(),
+  fullName: varchar("full_name", { length: 255 }).notNull(),
   email: text("email").notNull().unique(),
   universityId: integer("university_id").notNull().unique(),
-  password: text("password").notNull().unique(),
+  password: text("password").notNull(),
   universityCard: text("university_card").notNull(),
   status: STATUS_ENUM("status").default("PENDING"),
   role: ROLE_ENUM("role").default("USER"),
@@ -38,7 +38,7 @@ export const users = pgTable("users", {
 });
 
 export const books = pgTable("books", {
-  id: uuid().notNull().primaryKey().defaultRandom().unique(),
+  id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
   title: varchar("title", { length: 255 }).notNull(),
   author: varchar("author", { length: 255 }).notNull(),
   genre: text("genre").notNull(),
