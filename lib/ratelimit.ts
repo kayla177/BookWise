5051de38875c743b49c0e4df9f8d6c5ebcbab3a0
Kayla <likayla177@gmail.com>
2025-03-24 15:37:16 -0400

    upload api end point to custom domain

--- lib/ratelimit.ts
+++ lib/ratelimit.ts
@@ -2,7 +2,7 @@ import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import redis from "@/database/redis";
 
 const ratelimit = new Ratelimit({
-  // sets the Redis instance used to store and track API requests(Redis stores the request count per user/IP for rate limiting).
+  // set the Redis instance used to store and track API requests(Redis stores the request count per user/IP for rate limiting).
   redis: redis,
   limiter: Ratelimit.fixedWindow(5, "1m"),
   // tracks rate limit usage and statistics
